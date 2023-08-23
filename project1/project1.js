b6cfe7206700dfe46965ffcced2bc5a302e16f42
itjohan <johanlindkvist89@gmail.com>
2023-08-23 20:38:13 +0200

    initial project1

---
+++ project1/project1.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * @param {ImageData} bgImg The background image to be modified
+ * @param {ImageData} fgImg The foreground image
+ * @param {number} fgOpac The opacity of the foreground image
+ * @param {{x: number, y: number}} fgPos The position of the foreground image in pixels. It can be negative and (0,0) means the top-left pixels of the foreground and background are aligned
+ */
+function composite(bgImg, fgImg, fgOpac, fgPos) {}
