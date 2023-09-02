bce905a67f0601562fb2adb289917bd2f602a349
Johan Lindkvist <johanlindkvist89@gmail.com>
2023-09-02 07:50:28 +0000

    add scale and rotation matrices

--- project2/project2.js
+++ project2/project2.js
@@ -28,6 +28,45 @@ class Matrix {
   }
 }
 
+class ScaleMatrix {
+  #matrix = new Matrix(0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+  /**
+   * @param {number} s1
+   * @param {number} s2
+   */
+  constructor(s1, s2) {
+    this.s1 = s1;
+    this.s2 = s2;
+    this.#matrix.a = s1;
+    this.#matrix.e = s2;
+  }
+}
+
+class RotationMatrix {
+  #matrix = new Matrix(0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+  /**
+   * @param {number} degrees
+   */
+  constructor(degrees) {
+    this.degrees = degrees;
+  }
+
+  set degrees(degrees) {
+    this.degrees = degrees;
+    const radians = degrees * (Math.PI / 180);
+    this.#setMatrix(radians);
+  }
+
+  #setMatrix(radians) {
+    this.#matrix.a = Math.cos(radians);
+    this.#matrix.b = -Math.sin(radians);
+    this.#matrix.d = Math.sin(radians);
+    this.#matrix.e = Math.cos(radians);
+  }
+}
+
 /**
  * Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
  * The transformation first applies scale, then rotation, and finally translation.
