b8e169a944efb7b490df40acdbfb0db78ffd983b
Johan Lindkvist <johanlindkvist89@gmail.com>
2023-09-02 07:14:26 +0000

    add tsdoc

--- project2/project2.js
+++ project2/project2.js
@@ -1,15 +1,27 @@
-// Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
-// The transformation first applies scale, then rotation, and finally translation.
-// The given rotation value is in degrees.
-function GetTransform( positionX, positionY, rotation, scale )
-{
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+// @ts-check
+
+/**
+ * Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
+ * The transformation first applies scale, then rotation, and finally translation.
+ * The given rotation value is in degrees.
+ * @param {number} positionX
+ * @param {number} positionY
+ * @param {number} rotation
+ * @param {number} scale
+ * @returns {[number, number, number, number, number, number, number, number, number]}
+ */
+function GetTransform(positionX, positionY, rotation, scale) {
+  return [1, 0, 0, 0, 1, 0, 0, 0, 1];
 }
 
-// Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
-// The arguments are transformation matrices in the same format.
-// The returned transformation first applies trans1 and then trans2.
-function ApplyTransform( trans1, trans2 )
-{
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+/**
+ * Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
+ * The arguments are transformation matrices in the same format.
+ * The returned transformation first applies trans1 and then trans2.
+ * @param {number} trans1
+ * @param {number} trans2
+ * @returns
+ */
+function ApplyTransform(trans1, trans2) {
+  return [1, 0, 0, 0, 1, 0, 0, 0, 1];
 }
