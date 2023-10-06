2d1aeb88d22fc9e652bbe4986de9416a4586bb15
itjohan <johanlindkvist89@gmail.com>
2023-10-06 15:14:02 +0200

    finish project 3

--- project3/project3.js
+++ project3/project3.js
@@ -1,10 +1,7 @@
-// [TO-DO] Complete the implementation of the following class and the vertex shader below.
-
 class CurveDrawer {
   constructor() {
     this.prog = InitShaderProgram(curvesVS, curvesFS);
-    // [TO-DO] Other initializations should be done here.
-    // [TO-DO] This is a good place to get the locations of attributes and uniform variables.
+
     this.t = gl.getAttribLocation(this.prog, 't');
     this.mvp = gl.getUniformLocation(this.prog, 'mvp');
     this.p0 = gl.getUniformLocation(this.prog, 'p0');
@@ -18,24 +15,55 @@ class CurveDrawer {
     for (var i = 0; i < this.steps; ++i) {
       tv.push(i / (this.steps - 1));
     }
-    // [TO-DO] This is where you can create and set the contents of the vertex buffer object
-    // for the vertex attribute we need.
+
+    this.buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(tv), gl.STATIC_DRAW);
   }
   setViewport(width, height) {
-    // [TO-DO] This is where we should set the transformation matrix.
-    // [TO-DO] Do not forget to bind the program before you set a uniform variable value.
+    const trans = [
+      2 / width,
+      0,
+      0,
+      0,
+      0,
+      -2 / height,
+      0,
+      0,
+      0,
+      0,
+      1,
+      0,
+      -1,
+      1,
+      0,
+      1,
+    ];
     gl.useProgram(this.prog); // Bind the program
+    gl.uniformMatrix4fv(this.mvp, false, trans);
   }
   updatePoints(pt) {
-    // [TO-DO] The control points have changed, we must update corresponding uniform variables.
-    // [TO-DO] Do not forget to bind the program before you set a uniform variable value.
-    // [TO-DO] We can access the x and y coordinates of the i^th control points using
-    // var x = pt[i].getAttribute("cx");
-    // var y = pt[i].getAttribute("cy");
+    gl.useProgram(this.prog);
+
+    const x0 = pt[0].getAttribute('cx');
+    const y0 = pt[0].getAttribute('cy');
+    gl.uniform2f(this.p0, x0, y0);
+    const x1 = pt[1].getAttribute('cx');
+    const y1 = pt[1].getAttribute('cy');
+    gl.uniform2f(this.p1, x1, y1);
+    const x2 = pt[2].getAttribute('cx');
+    const y2 = pt[2].getAttribute('cy');
+    gl.uniform2f(this.p2, x2, y2);
+    const x3 = pt[3].getAttribute('cx');
+    const y3 = pt[3].getAttribute('cy');
+    gl.uniform2f(this.p3, x3, y3);
   }
   draw() {
-    // [TO-DO] This is where we give the command to draw the curve.
-    // [TO-DO] Do not forget to bind the program and set the vertex attribute.
+    gl.useProgram(this.prog);
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+    gl.enableVertexAttribArray(this.t);
+    gl.vertexAttribPointer(this.t, 1, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.LINE_STRIP, 0, this.steps);
   }
 }
 
@@ -49,8 +77,9 @@ var curvesVS = `
 	uniform vec2 p3;
 	void main()
 	{
-		// [TO-DO] Replace the following with the proper vertex shader code
-		gl_Position = vec4(0,0,0,1);
+    float x = p0.x * pow(1.0 - t, 3.0) + p1.x * 3.0 * t * pow(1.0 - t, 2.0) + p2.x * 3.0 * (1.0 - t) * pow(t, 2.0) + p3.x * pow(t, 3.0);
+    float y = p0.y * pow(1.0 - t, 3.0) + p1.y * 3.0 * t * pow(1.0 - t, 2.0) + p2.y * 3.0 * (1.0 - t) * pow(t, 2.0) + p3.y * pow(t, 3.0);
+		gl_Position = mvp * vec4(x, y, 0.0, 1.0);
 	}
 `;
 
